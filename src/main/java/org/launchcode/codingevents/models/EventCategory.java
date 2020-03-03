2f11b4b9e8dc1c6cd37b1efdc6867758dec51371
Vanessa <vangalepark@gmail.com>
2020-03-03 03:13:01 -0600

    made the event categories clickable so you only see those events

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,15 +1,19 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events= new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -20,6 +24,10 @@ public class EventCategory extends AbstractEntity {
     public String getName() {return name; }
 
     public void setName(String name) {this.name = name;}
+
+    public List<Event> getEvents() {
+        return events;
+    }
 
     @Override
     public String toString() {return name;}
